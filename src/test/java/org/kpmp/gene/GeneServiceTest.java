5456a23fd73e65a7d2d53fe145d247f28eeb4b43
Zach Wright <zwright@umich.edu>
2021-02-17 14:50:27 -0500

    KPMP-2432: AC improvements

--- src/test/java/org/kpmp/gene/GeneServiceTest.java
+++ src/test/java/org/kpmp/gene/GeneServiceTest.java
@@ -28,8 +28,18 @@ public class GeneServiceTest {
     public void testQuerySymbolAndAlias() throws Exception {
         MyGeneInfoResult result1 = new MyGeneInfoResult();
         List<MyGeneInfoHit> hits1 = new ArrayList<>();
-        MyGeneInfoHit hit1 = new MyGeneInfoHit();
-        hits1.add(hit1);
+        MyGeneInfoHit hit1a = new MyGeneInfoHit();
+        hit1a.setSymbol("a");
+        MyGeneInfoHit hit1b = new MyGeneInfoHit();
+        hit1b.setSymbol("b");
+        MyGeneInfoHit hit1c = new MyGeneInfoHit();
+        hit1c.setSymbol("z");
+        hits1.add(hit1b);
+        hits1.add(hit1a);
+        hits1.add(hit1c);
+        hits1.add(hit1c);
+        hits1.add(hit1c);
+        hits1.add(hit1c);
         result1.setHits(hits1);
         MyGeneInfoResult result2 = new MyGeneInfoResult();
         List<MyGeneInfoHit> hits2 = new ArrayList<>();
@@ -40,8 +50,9 @@ public class GeneServiceTest {
         Mockito.doReturn(result1).when(geneService2).query(GeneService.GET_MY_GENE_INFO_QUERY_SYMBOL, "query");
         Mockito.doReturn(result2).when(geneService2).query(GeneService.GET_MY_GENE_INFO_QUERY_ALIAS, "query");
         List<MyGeneInfoHit> results = geneService2.querySymbolAndAlias("query");
-        assertEquals(results.get(0), hit1);
-        assertEquals(results.get(1), hit2);
+        assertEquals(results.get(0), hit1a);
+        assertEquals(results.get(1), hit1b);
+        assertEquals(results.get(5), hit2);
     }
 
 }
