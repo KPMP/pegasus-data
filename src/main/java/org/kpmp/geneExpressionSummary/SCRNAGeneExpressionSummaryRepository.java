191272cb3b5a00a32443cf02d47a39fdb2f25631
Zach Wright <zwright@umich.edu>
2021-04-12 15:20:33 -0400

    KPMP-2603_Fixes: no null tissue types

--- src/main/java/org/kpmp/geneExpressionSummary/SCRNAGeneExpressionSummaryRepository.java
+++ src/main/java/org/kpmp/geneExpressionSummary/SCRNAGeneExpressionSummaryRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface SCRNAGeneExpressionSummaryRepository extends CrudRepository<SCRNAGeneExpressionExpressionSummaryValue, GeneExpressionId> {
 
-    @Query(value = "SELECT DISTINCT scc.cluster, scc.cluster_name, IF(isnull(scr.gene), null, scc.cell_count) as cell_count, scc.cluster_id as id, scr.tissue_type, IF(isnull(scr.gene), :geneSymbol, scr.gene) as gene, scr.p_val as p_val, scr.p_val_adj as p_val_adj, scr.fold_change, scr.pct_1, scr.pct_2, scr.specificity, scr.avg_exp as avg_exp " +
+    @Query(value = "SELECT DISTINCT scc.cluster, scc.cluster_name, IF(isnull(scr.gene), null, scc.cell_count) as cell_count, scc.cluster_id as id, IF(isnull(scr.tissue_type), :tissueType, scr.tissue_type) as tissue_type, IF(isnull(scr.gene), :geneSymbol, scr.gene) as gene, scr.p_val as p_val, scr.p_val_adj as p_val_adj, scr.fold_change, scr.pct_1, scr.pct_2, scr.specificity, scr.avg_exp as avg_exp " +
             "FROM sc_cluster_v scc " +
             "LEFT JOIN sc_rnaseq scr ON scc.cluster = scr.cluster AND scc.tissue_type = :tissueType AND scr.gene = :geneSymbol AND scr.tissue_type = :tissueType ORDER BY scr.p_val IS NULL, scr.p_val ASC", nativeQuery = true)
     List<SCRNAGeneExpressionExpressionSummaryValue> findByTissueAndGeneAllClusters(@Param("geneSymbol") String geneSymbol, @Param("tissueType") String tissueType);
