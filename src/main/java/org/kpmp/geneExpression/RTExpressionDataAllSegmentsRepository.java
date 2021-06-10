e677fafbf981a9abd85cb138e64416c682642dd2
Zach Wright <zwright@umich.edu>
2021-06-10 12:38:58 -0400

    KPMP-2485: Add tests

--- src/main/java/org/kpmp/geneExpression/RTExpressionDataAllSegmentsRepository.java
+++ src/main/java/org/kpmp/geneExpression/RTExpressionDataAllSegmentsRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Repository
 public interface RTExpressionDataAllSegmentsRepository extends CrudRepository<RTExpressionDataAllSegments, Integer> {
-    List<RTExpressionDataAllSegments> findByGeneSymbolAndTissueType(String geneSymbol, String tissueType);
     @Query(value = "SELECT rts.*, rsv.sample_count FROM rt_segments rts " +
             "LEFT JOIN rt_summary_v rsv ON rts.segment = rsv.segment AND rts.tissue_type = rsv.tissue_type " +
             "WHERE rts.tissue_type = :tissueType AND rts.gene_symbol = :geneSymbol", nativeQuery = true)
--- src/main/java/org/kpmp/geneExpression/RTExpressionDataGTIRepository.java
+++ src/main/java/org/kpmp/geneExpression/RTExpressionDataGTIRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface RTExpressionDataGTIRepository extends CrudRepository<RTExpressionDataGTI, Integer> {
-    List<RTExpressionDataGTI> findByGeneSymbolAndTissueType(String geneSymbol, String tissueType);
     @Query(value = "SELECT rtg.*, rsv.sample_count FROM rt_gti rtg " +
             "LEFT JOIN rt_summary_v rsv ON rtg.segment = rsv.segment AND rtg.tissue_type = rsv.tissue_type " +
             "WHERE rtg.tissue_type = :tissueType AND rtg.gene_symbol = :geneSymbol", nativeQuery = true)
