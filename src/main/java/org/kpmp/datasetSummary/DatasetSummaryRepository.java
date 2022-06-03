08995d17c3024f8cc353ab98e62d58282f1aea29
Alex Barkell <albarkel@umich.edu>
2022-06-02 20:29:38 -0400

    readd query

--- src/main/java/org/kpmp/datasetSummary/DatasetSummaryRepository.java
+++ src/main/java/org/kpmp/datasetSummary/DatasetSummaryRepository.java
@@ -9,7 +9,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DatasetSummaryRepository extends CrudRepository<DatasetSummaryValue, DatasetSummaryId> {
 
-  @Cacheable("tissueTypeCount")
-	@Query(value = "SELECT count(*) FROM sv_file_v WHERE tissue_type= "AKI" and data_type=: ")
-  Long getCountByTissueType(@Param("tissue_type") String tissue_type, @Param("data_type") String data_type);
+  @Cacheable("dataSummaryCount")
+  @Query(value = "SELECT count(*) FROM sv_file_v WHERE tissue_type= :tissue_type and data_type= :data_type")
+  Long getDataSummaryCount(@Param("tissue_type") String tissue_type, @Param("data_type") String data_type);
+
+
+  @Cacheable("dataSummaryCount")
+  @Query(value = "SELECT count(*) FROM sv_file_v WHERE data_type= :data_type")
+  Long getParticipantSummaryCount(@Param("data_type") String data_type);
 }
