7a2cd9fa02ef8aa18e9e4b84bece222d68d14a23
Zach Wright <zwright@umich.edu>
2022-11-08 14:45:57 -0500

    KPMP-3819: Unused imports

--- src/main/java/org/kpmp/Application.java
+++ src/main/java/org/kpmp/Application.java
@@ -1,16 +1,11 @@
 package org.kpmp;
 
-import com.hazelcast.spring.cache.HazelcastCacheManager;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
