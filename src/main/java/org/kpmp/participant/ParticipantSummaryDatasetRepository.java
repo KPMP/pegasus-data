b4e3b2089e51849337e32988e87bdaaa8cc96062
dert1129 <nathancreger1111@gmail.com>
2023-01-03 13:54:18 -0500

    Change to Long

--- src/main/java/org/kpmp/participant/ParticipantSummaryDatasetRepository.java
+++ src/main/java/org/kpmp/participant/ParticipantSummaryDatasetRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParticipantSummaryDatasetRepository extends CrudRepository<ParticipantSummaryDataset, Integer> {
+public interface ParticipantSummaryDatasetRepository extends CrudRepository<ParticipantSummaryDataset, Long> {
 
 	@Cacheable("participantByRedcap")
 	ParticipantSummaryDataset findByRedcapId(@Param("redcap_id") String redcapId);
