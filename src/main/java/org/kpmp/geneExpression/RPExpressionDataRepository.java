cc5a3b4d50168d8cb5ee47ec5e46c33652d309d8
Zach Wright <zwright@umich.edu>
2023-12-01 11:11:38 -0500

    KPMP-4434: handle multiple proteins

--- src/main/java/org/kpmp/geneExpression/RPExpressionDataRepository.java
+++ src/main/java/org/kpmp/geneExpression/RPExpressionDataRepository.java
@@ -13,7 +13,7 @@ public interface RPExpressionDataRepository extends CrudRepository<RPExpressionD
     @Query(value = "SELECT rpe.*, count(*) as sample_count FROM rp_expression rpe " +
             "JOIN rp_metadata rpm ON LOWER(rpe.region) = LOWER(rpm.tissue_region) " +
             "WHERE rpe.gene_symbol = :geneSymbol AND rpe.tissue_type = :tissueType " +
-            "GROUP BY rpe.region", nativeQuery = true)
+            "GROUP BY rpe.region AND rpe.accession", nativeQuery = true)
     List<RPExpressionData> findByGeneSymbolAndTissueTypeWithCounts(String geneSymbol, String tissueType);
     @Cacheable("rpExpCountByGene")
     @Query(value = "SELECT COUNT(*) FROM rp_expression WHERE gene_symbol = :gene", nativeQuery = true)
