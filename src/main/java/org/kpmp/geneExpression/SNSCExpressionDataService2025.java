bf666fd6101eeec506cf0d331c49e81c6b7e835b
HaneenT <haneen@umich.edu>
2025-08-13 14:46:37 -0400

    update SNSCExpressionDataService2025

--- src/main/java/org/kpmp/geneExpression/SNSCExpressionDataService2025.java
+++ src/main/java/org/kpmp/geneExpression/SNSCExpressionDataService2025.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class SNSCExpressionDataService2025 {
 
-	private SNExpressionDataRepository snRepo;
+	private SNExpressionDataRepository2025 snRepo;
 	private SCExpressionDataRepository2025 scRepo;
 
 	@Autowired
-	public SNSCExpressionDataService2025(SNExpressionDataRepository snRepo, SCExpressionDataRepository2025 scRepo) {
+	public SNSCExpressionDataService2025(SNExpressionDataRepository2025 snRepo, SCExpressionDataRepository2025 scRepo) {
 		this.snRepo = snRepo;
 		this.scRepo = scRepo;
 	}
 
 	public JSONObject getGeneExpressionValues(String dataType, String geneSymbol) throws JSONException, Exception {
 		if (dataType.equals(FullDataTypeEnum.SINGLE_NUCLEUS.getAbbreviation())) {
-			SNExpressionData expressionData = snRepo.findByGeneSymbol(geneSymbol);
+			SNExpressionData2025 expressionData = snRepo.findByGeneSymbol(geneSymbol);
 			return expressionData.getExpressionDataAsJson();
 		} else if (dataType.equals(FullDataTypeEnum.SINGLE_CELL.getAbbreviation())) {
 			SCExpressionData2025 expressionData = scRepo.findByGeneSymbol(geneSymbol);
